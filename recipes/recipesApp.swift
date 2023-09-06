637709e458f79068ed3d8ecf03c5adbcefc5fe41
pieceofpizza <lisanovadarina@gmail.com>
2023-09-06 17:54:31 +0300

    Initial Commit

---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by Дарина Лисанова on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
